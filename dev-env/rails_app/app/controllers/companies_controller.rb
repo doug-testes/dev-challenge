d5b0bfad07984d11dd40279e03e7a967a4a0c374
Douglas Lara <douglasdelaras@gmail.com>
2020-11-05 23:36:09 -0300

    Created views and controllers

---
+++ dev-env/rails_app/app/controllers/companies_controller.rb
@@ -0,0 +1,80 @@
+class CompaniesController < ApplicationController
+  before_action :set_company, only: [:show, :edit, :update, :destroy]
+
+  # GET /companies
+  # GET /companies.json
+  def index
+    @companies = Company.all
+  end
+
+  # GET /companies/1
+  # GET /companies/1.json
+  def show
+  end
+
+  # GET /companies/new
+  def new
+    @company = Company.new
+    @company.build_address
+  end
+
+  # GET /companies/1/edit
+  def edit
+  end
+
+  # POST /companies
+  # POST /companies.json
+  def create
+    @company = Company.new(company_params)
+    
+    borrower_request = BorrowerRequest.find(params[:borrower_request_id])
+    @company.borrower_request = borrower_request
+    
+
+    respond_to do |format|
+      if @company.save and (BorrowerService.new.generate_review(borrower_request).key?(:result))
+        format.html { redirect_to review_borrower_request_path(borrower_request), notice: "Company was successfully created." }
+        format.json { render :show, status: :created, location: @company }
+      else
+        format.html { render :new }
+        format.json { render json: @company.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /companies/1
+  # PATCH/PUT /companies/1.json
+  def update
+    respond_to do |format|
+      if @company.update(company_params)
+        format.html { redirect_to @company, notice: "Company was successfully updated." }
+        format.json { render :show, status: :ok, location: @company }
+      else
+        format.html { render :edit }
+        format.json { render json: @company.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /companies/1
+  # DELETE /companies/1.json
+  def destroy
+    @company.destroy
+    respond_to do |format|
+      format.html { redirect_to companies_url, notice: "Company was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_company
+    @company = Company.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def company_params
+    params.require(:company).permit(:cnpj, :phone, :name, address_attributes: [:postal_code, :state, :city, :neighbourhood, :street, :number, :complement])
+  end
+end
--- dev-env/rails_app/config/routes.rb
+++ dev-env/rails_app/config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  root "borrower_requests#new"
+  resources :borrower_requests, only: [:new, :show, :create] do
+    get "review", on: :member
+    post "approve", on: :member
+    resources :companies
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
