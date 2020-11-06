b09bce9b6ed6cd2c87ce09ff268326337199c8ac
Doug <douglasdelaras+impulso@gmail.com>
2020-11-06 03:10:38 -0300

    Change size for cents fields

--- dev-env/rails_app/db/migrate/20201104032426_create_borrower_installments.rb
+++ dev-env/rails_app/db/migrate/20201104032426_create_borrower_installments.rb
@@ -3,7 +3,7 @@ class CreateBorrowerInstallments < ActiveRecord::Migration[6.0]
     create_table :borrower_installments do |t|
       t.integer :status, default: 0, null: false
       t.references :borrower_request, null: false, foreign_key: true
-      t.integer :amount_in_cents, default: 0, null: false
+      t.integer :amount_in_cents, default: 0, null: false, limit: 8
       t.date :due_date, null: false
       t.integer :installment_number, default: 0, null: false
 
